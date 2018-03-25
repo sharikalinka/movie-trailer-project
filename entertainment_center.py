89b92eaf83eb5fa3c84a19b7905f6bfd923c607a
sharikalinka <35243063+sharikalinka@users.noreply.github.com>
2018-03-24 19:48:05 -0700

    Code reviewed PEP8 Compliant

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,50 +8,49 @@ declare Shari's favorite movies, with 4 arguments each:
 3 poster_url (poster image url)
 4 trailer_url (youtube trailer url)
 """
-harry_potter_philosophers_stone = media.Movie(
+philos = media.Movie(
     "Harry Potter and the Philosophers Stone",
     2001,
     "http://bit.ly/2FOMZ6f",
     "https://www.youtube.com/watch?v=PbdM1db3JbY")
-harry_potter_chamber_of_secrets = media.Movie(
+chamber = media.Movie(
     "Harry Potter and the Chamber of Secrets",
     2002,
     "http://bit.ly/2FIKwy3",
     "https://www.youtube.com/watch?v=r8_iuixRa_Y")
-harry_potter_prisoner_of_azkaban = media.Movie(
+prisoner = media.Movie(
     "Harry Potter and the Prisoner of Azkaban",
     2004,
     "http://bit.ly/2j6X3hv",
     "https://www.youtube.com/watch?v=R69laoH02xg")
-harry_potter_goblet_of_fire = media.Movie(
+goblet = media.Movie(
     "Harry Potter and the Goblet of Fire",
     2005,
     "http://bit.ly/2pfPYhI",
     "https://www.youtube.com/watch?v=WVNENtEJyMM")
-harry_potter_order_of_the_phoenix = media.Movie(
+phoenix = media.Movie(
     "Harry Potter and the Order of the Phoenix",
     2007,
     "http://bit.ly/2FWzGUu",
     "https://www.youtube.com/watch?v=WS8Jbm8Gob4")
-harry_potter_half_blood_prince = media.Movie(
+half = media.Movie(
     "Harry Potter and the Half-Blood Prince",
     2009,
     "http://bit.ly/2G1sNRW",
     "https://www.youtube.com/watch?v=JYLdTuL9Wjw&t=2s")
-harry_potter_deathly_hallows_1 = media.Movie(
+hallows1 = media.Movie(
     "Harry Potter and the Deathly Hallows – Part 1",
     2010,
     "http://bit.ly/2IqZmqF",
     "https://www.youtube.com/watch?v=4fG2Q1VCm7U")
-harry_potter_deathly_hallows_2 = media.Movie(
+hallows2 = media.Movie(
     "Harry Potter and the Deathly Hallows – Part 2",
     2011,
     "http://bit.ly/2GyKXbu",
     "https://www.youtube.com/watch?v=5NYt1qirBWg")
 
 # Assign each movie to a movie array
-movies = [harry_potter_philosophers_stone,harry_potter_chamber_of_secrets,harry_potter_prisoner_of_azkaban,harry_potter_goblet_of_fire,
-    harry_potter_order_of_the_phoenix,harry_potter_half_blood_prince,harry_potter_deathly_hallows_1,harry_potter_deathly_hallows_2]
+movies = [philos, chamber, prisoner, goblet, phoenix, half, hallows1, hallows2]
 
 # Call movie trailer page method, pass movie array, sort option
-fresh_tomatoes.open_movies_page(movies,"chronological")
+fresh_tomatoes.open_movies_page(movies, "chronological")
